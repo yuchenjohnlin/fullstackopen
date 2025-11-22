c79524bfccfe2c4d03691f1977b91dfe503c66aa
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-22 18:01:11 -0500

    part 2.8

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '000000000' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setName] = useState('')
+  const [newPhone, setPhone] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
-    setNewName(event.target.value)
+    setName(event.target.value)
+  }
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value)
+    setPhone(event.target.value)
   }
   const addName = (event) => {
     event.preventDefault()
@@ -19,11 +24,13 @@ const App = () => {
     }
 
     const nameObj = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
     setPersons(persons.concat(nameObj))
-    // console.log(persons) this doesn't actually display the new one because it doesn't update automatically
-    setNewName('')
+    console.log(persons) // this doesn't actually display the new one because it doesn't update automatically
+    setName('')
+    setPhone('')
   }
 
   return (
@@ -32,6 +39,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          phone number: <input value={newPhone} onChange={handlePhoneChange}/>
         </div>
         <div>
           <button type="submit">add</button>
