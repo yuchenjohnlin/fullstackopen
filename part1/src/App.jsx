19e642439b124358357052f02a1d083f787d172f
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-22 14:58:09 -0500

    part1.3&1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,17 +5,32 @@ const Title = ({text}) => <h1>{text}</h1>
 const Button = ({handleClick, text}) => {
 
   return (
-    <div>
+    <>
       <button onClick={handleClick}>{text}</button>
-    </div>
+    </>
   )
 }
 
-const setRandom = (setter, length) => () => {
-  const random = Math.floor(Math.random()*length) 
+const setRandom = (setter, length, previous) => () => {
+  let random = Math.floor(Math.random()*length) 
   console.log(random)
+  while(random === previous){
+    random = Math.floor(Math.random()*length)
+    console.log(random)
+  }
   setter( random)
 }
+
+const AnectodeDisplay = ({anecdotes, votes, index}) => {
+
+  return (
+    <>
+      <p>{anecdotes[index]}</p>
+      <p>has {votes[index]} votes</p>
+    </>
+  )
+} 
+
 
 const App = () => {
   const anecdotes = [
@@ -28,13 +43,33 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
    
   const [selected, setSelected] = useState(0)
+  const addVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    console.log(copy)
+  }
+
+  // Math.max doesn't accept arrays, it accepts individual numbers so use ...vote
+  const maxIndex = votes.indexOf(Math.max(...votes))
 
   return (
     <div>
-      {anecdotes[selected]}
-      <Button handleClick={setRandom(setSelected, anecdotes.length)} text={"next anectode"}/>
+      <Title text="Anecdote of the day"/>
+      
+      <AnectodeDisplay anecdotes={anecdotes} votes={votes} index={selected}/>
+      <div>
+        <Button handleClick={setRandom(setSelected, anecdotes.length, selected)} text={"next anectode"}/>
+        <Button handleClick={setRandom(addVote, votes.length)} text={"vote"}/>
+      </div>
+      <Title text="Anecdote with most votes"/>
+      <AnectodeDisplay anecdotes={anecdotes} votes={votes} index={maxIndex}/>
+
+
+
     </div>
   )
 }
