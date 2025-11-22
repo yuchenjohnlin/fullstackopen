3f9a9c05bf4e1bd382b40fd95f482808bf7919df
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-21 22:56:33 -0500

    finished refactoring for 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,46 +8,58 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
+  console.log(parts)
+  // const list = parts.map(value => <p>{value.name} {value.exercises}</p>)
+  // console.log(list)
+
+  // inline version is more idiomatic ? and because precomputed will require a key for each child element for the <p>
   return (
-  <div>
-    <p>{props.part1} {props.exercise1}</p>
-    <p>{props.part2} {props.exercise2}</p>
-    <p>{props.part3} {props.exercise3}</p>
-  </div>
+    <div>
+      {parts.map(p => (
+        <p key={p.name}>{p.name} {p.exercises}</p>
+      ))}
+      {/* {list} */}
+    </div>
   )
 };
 
-const Total = ({ex1,ex2,ex3}) => { 
-  <p>Number of exercises {ex1 + ex2 + ex3}</p>
+const Total = ({parts}) => { 
+  let sum = 0
+  parts.forEach(p => {sum += p.exercises || 0})
+
+  return (
+    <div>
+      <p>Number of exercises {sum}</p>
+    </div>
+  )
+    
 };
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const list = parts.map(part => part.exercises)
 
   return (
     <div>
       <Header course={course} />
 
-      <Content 
-        part1={part1.name} exercise1={part1.exercises}
-        part2={part2.name} exercise2={part2.exercises}
-        part3={part3.name} exercise3={part3.exercises}
-      />
-      <Total ex1={part1.exercises} ex2={part2.exercises} ex3={part3.exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts}  />
       
     </div>
   )
