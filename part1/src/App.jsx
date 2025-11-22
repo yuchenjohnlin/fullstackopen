18444c2753da41440fce5e382e6351708144652d
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-21 22:31:47 -0500

    finished refactoring for 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -24,23 +24,30 @@ const Total = ({ex1,ex2,ex3}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <Header course={course} />
 
       <Content 
-        part1={part1} exercise1={exercises1}
-        part2={part2} exercise2={exercises2}
-        part3={part3} exercise3={exercises3}
+        part1={part1.name} exercise1={part1.exercises}
+        part2={part2.name} exercise2={part2.exercises}
+        part3={part3.name} exercise3={part3.exercises}
       />
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+      <Total ex1={part1.exercises} ex2={part2.exercises} ex3={part3.exercises} />
       
     </div>
   )
