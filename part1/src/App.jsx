b20c22c3c23643dec37128e211aca0b60b862037
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-22 12:39:19 -0500

    finished refactoring for 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -37,29 +37,30 @@ const Total = ({parts}) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  const list = parts.map(part => part.exercises)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
 
-      <Content parts={parts}/>
-      <Total parts={parts}  />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}  />
       
     </div>
   )
