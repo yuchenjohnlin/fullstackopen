24d91a8e6af08eb4b408d4d961c97e5cb5f41109
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-22 21:59:55 -0500

    part2.12

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -10,9 +10,9 @@ const App = () => {
   const [newPhone, setPhone] = useState('')
   const [filterName, setFilterName] = useState('')
 
-
+  //Render phase (must be pure) ↓ Commit phase (update DOM) ↓ Effect phase (run useEffects)
+  // Well useEffect is to not have side effects that would interfere with the render process although I can't picture what side effects are
   useEffect( () => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/persons')      
       .then(response => {        
@@ -20,21 +20,17 @@ const App = () => {
         setPersons(response.data)      
       })
   }, [])  
-  console.log('render', persons.length, 'persons')
 
+  // if only 1 parameter then you can ignore the ()
+  // arrows can implicit ignore returns
+  const inputHandler = setter => event => {
+  // const inputHandler = setter => {
+    // return (event) => {
+    // console.log(event.target.value)
+    setter(event.target.value)
+    // }
+  }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setName(event.target.value)
-  }
-  const handlePhoneChange = (event) => {
-    console.log(event.target.value)
-    setPhone(event.target.value)
-  }
-  const handleFilterNameChange = (event) => {
-    console.log(event.target.value)
-    setFilterName(event.target.value)
-  }
   const addName = (event) => {
     // need to handle new id
     event.preventDefault()
@@ -46,21 +42,36 @@ const App = () => {
 
     const nameObj = {
       name: newName,
-      phone: newPhone,
+      number: newPhone,
       id: persons.length > 0 ? Math.max(...persons.map(p => p.id)) + 1 : 1
     }
-    setPersons(persons.concat(nameObj))
+    // setPersons(persons.concat(nameObj))
     console.log(persons) // this doesn't actually display the new one because it doesn't update automatically
     setName('')
     setPhone('')
+
+    // have to add the new content to the backend
+    // However, we shouldn't put effects in functions because hooks have to run on every render and functions are only activated sometimes
+    // and thing is that React loses track of it and it breaks the order -> invalid hook call
+    // it is because I copied that above in to this function, thinking that it might need it 
+    // but hooks are for side Effects that should run because of rendering Not because of clicking a button 
+    // Effects = things that reach outside of the React world:
+    axios
+      .post('http://localhost:3001/persons', nameObj)
+      // I would have to wait for the response to get it (mostly because the id issue but here we actually defined the ids ourselves)
+      .then(response => {
+        // because it became a [] 
+        setPersons(persons.concat(response.data) )
+        console.log(response.data)
+      })
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filterName={filterName} handleFilterNameChange={handleFilterNameChange}/>
+      <Filter filterName={filterName} handleFilterNameChange={inputHandler(setFilterName)}/>
       <h2>add a new</h2>
-      <PersonForm addName={addName} newName={newName} newPhone={newPhone} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange} />
+      <PersonForm addName={addName} newName={newName} newPhone={newPhone} handleNameChange={inputHandler(setName)} handlePhoneChange={inputHandler(setPhone)} />
       <h2>Numbers</h2>
       <Phonebook persons={persons} filter={filterName} />
     </div>
--- part2/src/Phonebook.jsx
+++ part2/src/Phonebook.jsx
@@ -1,6 +1,9 @@
 const Phonebook = ({persons, filter}) => {
-  const filterPersons = persons.filter(p => p.name.toLowerCase().startsWith(filter) )
-  console.log(filterPersons)
+    // if a person doesn't have a name (not inputed) then it cannopt do lowercase and will return error
+    console.log("Persons", persons)
+    console.log("filter", filter)
+  const filterPersons = persons.filter(p => p.name && p.name.toLowerCase().startsWith(filter) )
+    console.log("filterPersons", filterPersons)
   const list = filterPersons.map(person => <p key={person.id}>{person.name} {person.number}</p>)
 
   return (
