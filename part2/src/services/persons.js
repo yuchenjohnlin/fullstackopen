dc6adefc3964f2a318a838715bef5b10dfaef1bd
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-24 16:30:07 -0500

    part3.9-3.11, to checkout the app please go to PhonebookApp repo

--- part2/src/services/persons.js
+++ part2/src/services/persons.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // const baseUrl = 'http://localhost:3001/persons'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
-
-app.use(cors())
+app.use(express.static('dist'))
+// app.use(cors())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
 app.use(morgan('tiny'))
