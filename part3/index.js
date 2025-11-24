337f895442a8445db7488d2e037ab32977a83e17
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-24 13:43:11 -0500

    part3.7&3.8

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
