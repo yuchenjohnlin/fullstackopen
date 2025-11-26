b32c36102d54aeb591be6dffe6aab29d5dcb680c
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-26 12:32:23 -0500

    part4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -15,7 +15,23 @@ const totalLikes = (blogs) => {
     return sum(blogs)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (max, item) => {
+        return max.likes < item.likes ? item : max
+    }
+
+    // return blogs.reduce(reducer, -1)
+    // we shouldn't set max = -1, it wouldn't break because comparing two different objects 
+    // will return false anyways, so the comparison is always false
+    // if we don;t have this condition then we will be doping reduce on an empty list,
+    // error : Reduce of empty array with no initial value
+    return blogs.length === 0
+        ? null
+        : blogs.reduce(reducer)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
