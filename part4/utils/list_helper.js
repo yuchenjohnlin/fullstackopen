9055675e383f8559a95a73b8dea05ac7399e925b
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-11-26 12:04:54 -0500

    part4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
     return 1
 }
 
+const sum = array => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return array.length === 0 
+        ? 0
+        : array.reduce(reducer, 0)
+}
+
+const totalLikes = (blogs) => {
+    return sum(blogs)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
