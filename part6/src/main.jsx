4e62d086ee4227ef5baa53db3852e7ac7c50ec4c
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-12-01 17:10:30 -0500

    part6.1&6.2

---
+++ part6/src/main.jsx
@@ -0,0 +1,28 @@
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import counterReducer from './reducers/counterReducer'
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'RESET' })}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
